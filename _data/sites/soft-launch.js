7e7f0490192e1e74dfe75862e0f5ad9361a967d8
Ludovic Taffin <Drumor@users.noreply.github.com>
2024-06-13 16:14:51 +0200

    Update soft-launch.js

--- _data/sites/soft-launch.js
+++ _data/sites/soft-launch.js
@@ -5,8 +5,8 @@ module.exports = {
 		frequency: 1, // (in minutes)
     },
     urls: [
-      "https://soft-launch.lolaliza.com",
-      "https://soft-launch.lolaliza.com/be-en/kleding/jurken/1101",
-      "https://soft-launch.lolaliza.com/be-en/wikkeljurk_met_ceintuur-red_lollipop-08103199_5301.html",
+      "https://soft-launch-staging.lolaliza.com",
+      "https://soft-launch-staging.lolaliza.com/be-en/kleding/jurken/1101",
+      "https://soft-launch-staging.lolaliza.com/be-en/wikkeljurk_met_ceintuur-red_lollipop-08103199_5301.html",
     ]
 };
