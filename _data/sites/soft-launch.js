67f3aa2e15eb8966b3262eca7256c5b5b7aba64b
Ludovic Taffin <Drumor@users.noreply.github.com>
2024-06-13 15:46:41 +0200

    Create soft-launch.js
    add prod config

---
+++ _data/sites/soft-launch.js
@@ -0,0 +1,12 @@
+module.exports = {
+    description: "soft-launch.lolaliza.com",
+    options: {
+        runs: 1,
+		frequency: 1, // (in minutes)
+    },
+    urls: [
+      "https://soft-launch.lolaliza.com",
+      "https://soft-launch.lolaliza.com/be-en/kleding/jurken/1101",
+      "https://soft-launch.lolaliza.com/be-en/wikkeljurk_met_ceintuur-red_lollipop-08103199_5301.html",
+    ]
+};
